0af025270e6d01d5bb9d50584822f33ec599b211
Ernesto Urrutia <ernesto.urrutia.garcia@outlook.com>
2024-11-12 19:49:08 -0600

    12Nov24

--- UpdateDeepControl/obj/Debug/net8.0-windows/UpdateDeepControl.AssemblyInfo.cs
+++ UpdateDeepControl/obj/Debug/net8.0-windows/UpdateDeepControl.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpdateDeepControl")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2efabb415404fd743bca57208b1eec4f55f82258")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpdateDeepControl")]
